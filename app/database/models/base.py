26d50d0adc3a8bdf1196d04e10db66e67f3f55f0
Gleb Mikhalev <gleb1@bk.ru>
2025-09-17 14:09:25 +0300

    fixes typos

--- app/database/models/base.py
+++ app/database/models/base.py
@@ -7,15 +7,13 @@ from app.core.config import settings
 class Base(DeclarativeBase):
     __abstract__ = True
 
-    metadata = MetaData(
-        naming_convention=settings.db.naming_convention,
-    )
+    metadata = MetaData(naming_convention=settings.db.naming_convention)
 
     @declared_attr.directive
     @classmethod
     def __tablename__(cls) -> str:
         """
-        Get the tablename from the class name.
+        Get the tablename from the classname.
 
         Returns:
             str: tablename
